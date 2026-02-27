45b8557167966a6462af81806401f042711d48ec
Arslan-Choudary <arslanchoudary23@gmail.com>
2026-02-27 15:10:23 +0500

    trying

--- src/components/ui/chart.tsx
+++ src/components/ui/chart.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import * as RechartsPrimitive from "recharts"
-import { cn } from "@/lib/utils"
+import { cn } from "../../lib/utils"
 
 export type ChartConfig = Record<string, { label?: React.ReactNode; color?: string }>
 
